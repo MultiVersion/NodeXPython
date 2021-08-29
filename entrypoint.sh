30e3fa40955e47bafdfe3a670b34e32bd47efa27
Dviih <dviih@dviih.email>
2021-08-29 08:47:50 -0300

    Updated Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,21 @@
 #! /bin/bash
-python3 -m venv nodejs
+if [[ ${D_FRAMEWORK,,} == "nodejs*" ]]; then
+    if [[ ! -d node_modules ]]; then
+      npm install
+    fi
+    exec "node $@"
+fi
+
+if [[ ${D_FRAMEWORK,,} == "python3" ]]; then
+  if [[ -f requirements.txt ]]; then
+    pip install -r requirements.txt
+  fi
+  exec "python $@"
+fi
+
+if [[ ${D_FRAMEWORK,,} == "python2" ]]; then
+  if [[ -f requirements.txt ]]; then
+    pip install -r requirements.txt
+  fi
+  exec "python2 $@"
+fi
